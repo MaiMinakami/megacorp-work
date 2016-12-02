3dfe8eb6f6d50971eaa15d255e21a524eb5f68bf
MaiMinakami <irushkachka@gmail.com>
2016-12-02 21:18:00 +0200

    create megacorp

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $('.your-class').slick({        
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      fade: true,
+      cssEase: 'linear'
+  }); 
+console.log('111111111');
+
+});
